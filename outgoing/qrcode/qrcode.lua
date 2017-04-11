4eb400cd1f688e6ea6551f8a2532e73873cf8048
loddit <loddit@gmail.com>
2017-04-11 18:52:50 +0800

    add url encode logic

--- outgoing/qrcode/qrcode.lua
+++ outgoing/qrcode/qrcode.lua
@@ -1,14 +1,25 @@
+function urlencode(str)
+  if (str) then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w ])",
+    function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = string.gsub (str, " ", "+")
+  end
+  return str
+end
+
 text = json.parse(request.body).text
-URL = string.sub(text, string.find(text,"%s.+$"))
-URL = string.gsub(URL , "%s", "")
-if URL then
+url = string.sub(text, string.find(text,"%s.+$"))
+url = string.gsub(url , "%s", "")
+encoded_url = urlencode(url)
+if url then
   return {
-    text=URL,
+    text=url,
     attachments={
       {
         images={
           {
-            url="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="..URL,
+            url="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="..encoded_url,
             height=200,
             width=200
           }
